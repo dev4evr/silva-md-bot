9189ec50aeade02f7422aaa6e397bc3b7bd04caa
IsHaQ VII <90872109+dev4evr@users.noreply.github.com>
2026-02-18 15:01:03 +0000

    Fix quotes and syntax in index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require( express );
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get( / , (req, res) => {
-    res.send( Silva MD Bot is running 🚀 );
+app.get('/', (req, res) => {
+    res.send('Silva MD Bot is running 🚀');
 });
 
 app.listen(PORT, () => {
